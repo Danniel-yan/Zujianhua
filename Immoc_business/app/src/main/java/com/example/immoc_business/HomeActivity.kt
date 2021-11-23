092532d916bbe315b3852836b56bbec347ff72ca
yanshirong <yanshirong@shanda.com>
2021-11-23 13:47:24 +0800

    首页框架大家、maven私服搭建

---
+++ Immoc_business/app/src/main/java/com/example/immoc_business/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.immoc_business
+
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Created by yanshirong on 2021/11/23.
+ * E-Mail:yanshirong@shanda.com
+ * Description:
+ */
+class HomeActivity : FragmentActivity() {
+
+
+}
